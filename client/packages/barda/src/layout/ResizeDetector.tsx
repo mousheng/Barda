912936482e3eddf59da8eb9672764d45895937c9
moushengkoo <10402885@qq.com>
2025-12-15 21:46:55 +0800

    refactor: 标记MyResizeDetector为废弃，建议使用react-resize-detector替代

--- client/packages/barda/src/layout/ResizeDetector.tsx
+++ client/packages/barda/src/layout/ResizeDetector.tsx
@@ -4,9 +4,10 @@ import { useResizeDetector, useResizeDetectorProps } from 'react-resize-detector
 interface MyResizeDetectorProps extends useResizeDetectorProps<HTMLDivElement> {
     children?: ReactNode;
 }
+// @deprecated 准备废弃，使用react-resize-detector代替
 export const MyResizeDetector = (props: MyResizeDetectorProps) => {
     const { ref } = useResizeDetector(props);
-    return <div ref={ref} style={{ width: '100%', height: "100%" }} onResize={props.onResize as any}>{
+    return <div ref={ref} style={{ width: '100%', height: "100%" }}>{
         props.children
     }</div>;
 };
